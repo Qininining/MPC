cd439d857ab102fc8878fb66410fd45533e80069
Qininining <204747508@qq.com>
2025-06-21 15:37:15 +0800

    添加图表视图并更新主窗口布局，优化显示效果

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,22 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    BlueSeries = new QLineSeries();
+    BlueSeries->setColor(Qt::blue);
+    RedSeries = new QLineSeries();
+    RedSeries->setColor(Qt::red);
+    GreenSeries = new QLineSeries();
+    GreenSeries->setColor(Qt::green);
+
+
+    chart = new QChart();
+    chart->legend()->hide(); // 隐藏图例
+    chart->addSeries(BlueSeries);
+    chart->createDefaultAxes(); // 创建默认轴
+    chart->setTitle("Force (μN)");
+    ui->charts->setChart(chart);
+    ui->charts->setRenderHint(QPainter::Antialiasing); // 抗锯齿
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLineSeries>
+#include <QChart>
+
+#include "MPC.h" // 包含 MPC 类头文件
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +21,14 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+
 private:
     Ui::MainWindow *ui;
+
+    QLineSeries *BlueSeries;  // 用于绘制实际系统输出
+    QLineSeries *RedSeries;   // 用于绘制参考轨迹
+    QLineSeries *GreenSeries; // 用于绘制控制输入（可选）
+
+    QChart *chart;
 };
 #endif // MAINWINDOW_H
